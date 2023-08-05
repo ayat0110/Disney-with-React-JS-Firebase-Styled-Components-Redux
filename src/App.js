479ca818d5b7447925bcf900003ae3a10d9fc274
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-05 14:34:01 +0300

    initial commit😜🤪

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
