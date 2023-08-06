59b78636348840929791d5d56a3268726e1b6e21
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-06 19:20:47 +0300

    updated the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import styled from "styled-components"
 
 const Header = (props) => {
-    return <Nav>Header</Nav>
+    return <Nav>
+        <Logo>
+            <img src="/images/logo.svg" alt="Disney+" />
+        </Logo>
+        <NavMenu>menu</NavMenu>
+    </Nav>
 }
 const Nav = styled.nav`
 position:fixed;
@@ -16,5 +21,30 @@ align-items: center;
 padding: 0 36px;
 letter-spacing:16px;
 z-index:3;
+`
+const Logo =styled.a`
+padding: 0;
+width: 80px;
+margin-top: 4px;
+max-height: 70px;
+font-size:0;
+display:inline-block;
+
+img{
+    display:block;
+    width:100%
+}
+`
+const NavMenu = styled.div`
+align-items: center;
+display:flex;
+flex-flow:row nowrap;
+height:100%;
+justify-content:flex-end;
+margin: 0px;
+padding: 0px;
+position:relative;
+margin-right:auto;
+margin-left: 25px;
 `
 export default Header;
