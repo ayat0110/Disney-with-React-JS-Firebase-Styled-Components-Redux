407ea2cc6e5fc2ff61bf2386a69b782bd26ef3b5
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-06 22:00:45 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,73 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
 
 const Header = (props) => {
-    return <Nav>
-        <Logo>
-            <img src="/images/logo.svg" alt="Disney+" />
-        </Logo>
-        <NavMenu>
-            <a href="/home">
-                <img src="/images/home-icon.svg" alt="HOME" />
-                <span>HOME</span>
-            </a>
-            <a>
-                <img src="/images/search-icon.svg" alt="SEARCH" />
-                <span>SEARCH</span>
-            </a>
-            <a>
-                <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-                <span>WATCHLIST</span>
-            </a>
-            <a>
-                <img src="/images/original-icon.svg" alt="ORIGINALS" />
-                <span>ORIGINALS</span>
-            </a>
-            <a>
-                <img src="/images/movie-icon.svg" alt="MOVIES" />
-                <span>MOVIES</span>
-            </a>
-            <a>
-                <img src="/images/series-icon.svg" alt="SERIES" />
-                <span>SERIES</span>
-            </a>
-        </NavMenu>
-    </Nav>
-}
-const Nav = styled.nav`
-position:fixed;
-top: 0;
-left: 0;
-right: 0;
-height: 70px;
-background-color: #090b13;
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 0 36px;
-letter-spacing:16px;
-z-index:3;
-`
-const Logo =styled.a`
-padding: 0;
-width: 80px;
-margin-top: 4px;
-max-height: 70px;
-font-size:0;
-display:inline-block;
+  return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt="Disney+" />
+            </Logo>
 
-img{
-    display:block;
-    width:100%
-}
-`
+                    <NavMenu>
+                        <a href="/home">
+                            <img src="/images/home-icon.svg" alt="HOME" />
+                            <span>HOME</span>
+                        </a>
+                        <a>
+                            <img src="/images/search-icon.svg" alt="SEARCH" />
+                            <span>SEARCH</span>
+                        </a>
+                        <a>
+                            <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                            <span>WATCHLIST</span>
+                        </a>
+                        <a>
+                            <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                            <span>ORIGINALS</span>
+                        </a>
+                        <a>
+                            <img src="/images/movie-icon.svg" alt="MOVIES" />
+                            <span>MOVIES</span>
+                        </a>
+                        <a>
+                            <img src="/images/series-icon.svg" alt="SERIES" />
+                            <span>SERIES</span>
+                        </a>
+                    </NavMenu>
+          <Login >Login</Login>
+        </Nav>
+    );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
 const NavMenu = styled.div`
   align-items: center;
   display: flex;
@@ -124,4 +132,63 @@ const NavMenu = styled.div`
     display: none;
   } */
 `;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+const UserImg = styled.img`
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
 export default Header;
