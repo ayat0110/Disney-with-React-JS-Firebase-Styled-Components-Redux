811fc4c979158a4a07d42a11e83a1502f58a64df
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-08 18:43:05 +0300

    Update userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -5,3 +5,21 @@ const initialState = {
     email: "",
     photo: "",
 };
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUserLoginDetails: (state, action) => {
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+            state.photo = action.payload.photo;
+        },
+
+        setSignOutState: (state) => {
+            state.name = null;
+            state.email = null;
+            state.photo = null;
+        },
+    },
+});
