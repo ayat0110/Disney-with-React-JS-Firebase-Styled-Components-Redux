14c7574f32cd160a25bd3b330aa6554a719c5a4f
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-08 19:49:08 +0300

    bugs fixed

--- src/app/store.js
+++ src/app/store.js
@@ -3,10 +3,10 @@ import logger from 'redux-logger';
 import userReducer from "../features/user/userSlice";
 import movieReducer from "../features/movie/movieSlice";
 
-export default configureStore({
-    reducer: {
-        user: userReducer,
-        movie: movieReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-});
+ export default configureStore({
+     reducer: {
+         user: userReducer,
+         movie: movieReducer,
+     },
+     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+ });
