966a92cf9c742e5330a3dd3611aa15a3e3f76531
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-08 19:33:43 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,43 +37,45 @@ const Header = (props) => {
   };
 
   return (
-        <Nav>
-            <Logo>
-                <img src="/images/logo.svg" alt="Disney+" />
-            </Logo>
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+
       {!userName ? (
         <Login onClick={handleAuth}>Login</Login>
-     ) : (
+      ) : (
         <>
-            <NavMenu>
-                        <a href="/home">
-                            <img src="/images/home-icon.svg" alt="HOME" />
-                            <span>HOME</span>
-                        </a>
-                        <a>
-                            <img src="/images/search-icon.svg" alt="SEARCH" />
-                            <span>SEARCH</span>
-                        </a>
-                        <a>
-                            <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-                            <span>WATCHLIST</span>
-                        </a>
-                        <a>
-                            <img src="/images/original-icon.svg" alt="ORIGINALS" />
-                            <span>ORIGINALS</span>
-                        </a>
-                        <a>
-                            <img src="/images/movie-icon.svg" alt="MOVIES" />
-                            <span>MOVIES</span>
-                        </a>
-                        <a>
-                            <img src="/images/series-icon.svg" alt="SERIES" />
-                            <span>SERIES</span>
-                        </a>
-                    </NavMenu>
-          <Login onClick={handleAuth}>Login</Login>
-        </Nav>
-    );
+          <NavMenu>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="HOME" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="SEARCH" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="ORIGINALS" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="MOVIES" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="SERIES" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+        </>
+      )}
+    </Nav>
+  );
 };
 
 const Nav = styled.nav`
